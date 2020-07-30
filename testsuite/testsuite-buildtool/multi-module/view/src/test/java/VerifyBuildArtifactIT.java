18b78140a5792f2bf91f149d6927213cdcaa2cb8
penehyba <penehyba@redhat.com>
2020-07-30 16:14:01 +0200

    Archive<?>  and typo

--- testsuite/testsuite-buildtool/multi-module/view/src/test/java/VerifyBuildArtifactIT.java
+++ testsuite/testsuite-buildtool/multi-module/view/src/test/java/VerifyBuildArtifactIT.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import org.apache.http.client.fluent.Request;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -8,20 +10,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
 
-import java.io.File;
-
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Arquillian.class)
-public class VerifyBuildArtefactIT {
+public class VerifyBuildArtifactIT {
     @Deployment
-    public static Archive createDeployment() {
+    public static Archive<?> createDeployment() {
         return ShrinkWrap.createFromZipFile(JAXRSArchive.class, new File("target/endpoint.war"));
     }
 
     @Test
     @RunAsClient
-    public void testEndppoint() throws Exception {
+    public void testEndpoint() throws Exception {
         String response = Request.Get("http://127.0.0.1:8080/").execute().returnContent().asString();
         assertEquals("something", response);
     }
